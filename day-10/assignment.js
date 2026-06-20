a538e9db8ab98d5c5ebc13dfd2fb13fcce931488
Mohammed Bakhar <mrbakhar8686@gmail.com>
2026-06-20 17:07:17 +0530

    day 10 assignment and notes

---
+++ day-10/assignment.js
@@ -0,0 +1,71 @@
+// let mainHeading = document.getElementById("mainHeading");
+// console.log(mainHeading)
+
+// let introPara = document.getElementById("intro");
+// console.log(introPara);
+
+
+// let skill = document.getElementById("skillsList");
+// console.log(skill)
+
+// const variable = document.getElementById("gallery");
+// console.log(variable)
+
+// let footer = document.getElementById("pageFooter");
+// console.log(footer) 
+
+
+// part 2
+
+
+
+// const para = document.getElementsByClassName("description");
+// console.log(para)
+// console.log(para.length)
+
+// let list = document.getElementsByClassName("skill");
+// console.log(list.length)
+
+// const images = document.getElementsByClassName("project");
+// console.log(images)
+
+// const individualElement = document.getElementsByClassName("description");
+// console.log(individualElement[0]);
+// console.log(individualElement[1]);
+
+
+// let  skill = document.getElementsByClassName("skill");
+// for (let index = 0; index < skill.length; index++) {
+//     const element = skill[index];
+//     console.log(element)   
+// }
+
+
+// part 3
+
+
+// let para = document.getElementsByTagName("p");
+// console.log(para.length)
+
+
+// let head2 = document.getElementsByTagName("h2");
+// console.log(head2)
+
+
+// let allImg = document.getElementsByTagName("img");
+// console.log(allImg)
+
+// let firstPara = document.getElementsByTagName("p");
+// console.log(firstPara[0])
+
+
+
+// part 4
+
+
+// let mainHeading = document.querySelector("#mainHeading");
+// console.log(mainHeading)
+
+
+let para = document.querySelector(".description");
+console.log(para)
---
+++ day-10/notes.js
@@ -0,0 +1,58 @@
+// console.dir(document)
+
+
+// let mainImage = document.getElementById("mainImg");
+// console.log(mainImage);
+
+
+// let element = document.getElementById("nonexistent");
+// console.log(element)
+
+
+// let oldImages = document.getElementsByClassName("oldImg");
+// console.log(oldImages);
+// console.log(oldImages.length);
+
+
+// let oldImages = document.getElementsByClassName("oldImg");
+// // console.log(oldImages[0]); // First image
+// // console.log(oldImages[1]); // Second image
+// // console.log(oldImages[2]); // Third image
+
+// for (let i = 0; i < oldImages.length; i++) {
+//    console.log(oldImages[i])
+// }
+
+
+// let boxLinks = document.getElementsByClassName("boxLink");
+// console.log(boxLinks);
+
+
+// let paragraphs = document.getElementsByTagName("p");
+// console.log(paragraphs);
+
+// let paragraphs = document.getElementsByTagName("p");
+// console.log(paragraphs.length);
+
+
+// let allLinks = document.getElementsByTagName("a");
+// console.log(allLinks);
+// // Output: HTMLCollection of all <a> tags on the page
+// console.log(allLinks.length); // Total number of links
+
+
+
+// let allImages = document.getElementsByTagName("img");
+// console.log(allImages)
+
+
+// let heading = document.querySelector("#heading1");
+// console.log(heading);
+
+
+// let firstOldImage = document.querySelector(".oldImg");
+// console.log(firstOldImage);
+
+
+// let allOldImages = document.querySelectorAll(".oldImg");
+// console.log(allOldImages);
